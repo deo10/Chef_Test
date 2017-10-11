1f41206c340c63d25235488ee8c59db347f1d4ec
EC2 Default User <ec2-user@ip-172-31-42-252.ec2.internal>
2017-10-11 16:39:02 +0000

    Metadata update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures learn-chef'
 long_description 'Installs/Configures learn-chef'
-version '0.1.0'
+version '0.2222222222222222222222.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
